d894fe8454a9cdf4baefd2bb72a55078a8ee501c
Uchiha-SR <raoranesagar5@gmail.com>
2023-08-29 11:44:41 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Intro to state in Compose"
+include(":app")
+ 
